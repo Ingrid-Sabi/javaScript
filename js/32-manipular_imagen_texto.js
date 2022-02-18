09fd03eb1aa0ffacdacb62185a0651b6c6d78997
ingrid-sabi <ingridvanesasabicharry@gmail.com>
2022-02-18 11:55:27 -0500

    cambioimagen

---
+++ js/32-manipular_imagen_texto.js
@@ -0,0 +1,18 @@
+"use stript"
+
+
+function ocultarImagen(){
+
+    document.getElementById("ironman").className= "hidden";
+  
+}
+function aparecer(){
+
+    document.getElementById("ironman").className= "visible      ";
+  
+}
+function cambiar(){
+
+    document.getElementById("terror").src = "img/tokio3.jpg";
+  
+}
